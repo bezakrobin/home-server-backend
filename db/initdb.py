d48a20c0c461c905caccd08ea15e464119881fb6
Robin Bezak <bezakrobin@gmail.com>
2023-10-19 01:19:06 +0200

    removed services

--- db/initdb.py
+++ db/initdb.py
@@ -5,18 +5,6 @@ def init_database(database_name='db/db.db'):
     with sqlite3.connect(database_name) as conn:
         cursor = conn.cursor()
 
-        create_services_table_sql = '''
-            CREATE TABLE IF NOT EXISTS services (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                svg_data TEXT,
-                active BOOLEAN DEFAULT 0,
-                created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )
-        '''
-
-        cursor.execute(create_services_table_sql)
-
         create_tasks_table_sql = '''
             CREATE TABLE IF NOT EXISTS tasks (
                 id INTEGER PRIMARY KEY,
--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from routes.services import services_bp
 from routes.tasks import tasks_bp
 from db.initdb import init_database
 
 app = Flask(__name__)
 
-app.register_blueprint(services_bp)
 app.register_blueprint(tasks_bp)
 
 if __name__ == '__main__':
